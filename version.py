6144f2d4e3258d08f2665cc93c2d24a18b992fa4
mrwise-max <nabilelhilaly@gmail.com>
2026-03-18 12:52:15 +0000

    Remove source files - releases only repo

--- version.py
+++
@@ -1,7 +0,0 @@
-"""Version information for Nabil Video Studio Pro"""
-
-VERSION = "1.7.17"
-VERSION_TUPLE = (1, 7, 17, 0)
-APP_NAME = "Nabil Video Studio Pro"
-AUTHOR = "Nabil Software"
-COPYRIGHT = "Copyright (C) 2025 Nabil Software"
